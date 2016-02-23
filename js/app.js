1e289473fbded4b51066efecb20a649ef989f1f5
Jessica Hathaway <hathaway.jess@gmail.com>
2016-02-22 18:07:09 -0700

    Finished

--- js/app.js
+++ js/app.js
@@ -4,6 +4,28 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
     $httpProvider.interceptors.push('httpRequestInterceptor');
 
-    // routing configuration code
+    $stateProvider
+      .state('home', {
+        url: '/' ,
+        controller: 'homeCtrl' ,
+        templateUrl: 'js/home/homeTmpl.html' ,
+        resolve: {
+                allTeamData: function(homeService) {
+                  return homeService.getAllTeamData();
+                }
+        }
+      })
+      .state('teams', {
+        url: '/teams/:team' ,
+        controller: 'teamCtrl' ,
+        templateUrl: 'js/teams/teamTmpl.html' ,
+        resolve: {
+                  teamData: function(teamService, $stateParams) {
+                    return teamService.getTeamData($stateParams.team);
+                  }
+            }
+      })
+
+      $urlRouterProvider.otherwise('/');
 
 });
